6d80ee7e57ac1f223ddffc5d063f00b2b364faff
urandom11 <this.is.incorrect.login@gmail.com>
2022-02-27 16:11:33 +0000

    minor changes

--- scan.sh
+++ scan.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -e "CIDR file: $1 | rate: $2 (pps) | timeout: $3"
+echo -e "CIDR file: $1 | rate: $2 (pps) | timeout: $3 | portlist: $4"
+
+RESDIR="results"
 
 while true
 do
@@ -8,13 +10,22 @@ do
 		timeout -s SIGINT $3 masscan --resume paused.conf
 		echo -e "timeout exceeded, sleeping for next 3 seconds"
 		sleep 3
-		masscan --readscan $1_result.bin -oX $1_result.xml
-		masscan --readscan $1_result.bin -oG $1_result.txt
+		masscan --readscan $1_result.bin -oX $RESDIR/$(basename $1).xml
+		masscan --readscan $1_result.bin -oG $RESDIR/$(basename $1).txt
+		echo -e "github push..."
+		git add --all $RESDIR/
+		git commit -m "scan results update"
+		git push -u origin main
 	else
-		timeout -s SIGINT $3 masscan -iL $1 -p3389,80,443 --rate=$2 -oB $1_result.bin
+		timeout -s SIGINT $3 masscan -iL $1 -p$4 --rate=$2 -oB $1_result.bin
 		echo -e "timeout exceeded, sleeping for next 3 seconds"
 		sleep 3
-		masscan --readscan $1_result.bin -oX $1_result.xml
-		masscan --readscan $1_result.bin -oG $1_result.txt
+		masscan --readscan $1_result.bin -oX $RESDIR/$(basename $1).xml
+		masscan --readscan $1_result.bin -oG $RESDIR/$(basename $1).txt
+		echo -e "github push..."
+		git add --all $RESDIR/
+		git commit -m "scan results update"
+		git push -u origin main
 	fi
 done
+exit 0
